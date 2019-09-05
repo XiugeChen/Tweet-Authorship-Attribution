5ca80e98b07b4ca0cd0ed829732468cb04ff0e61
An (Martin) Luo <aluo1.developer@gmail.com>
2019-09-05 16:52:33 +1000

    complete shuffle_matrix()

--- src/cnn.py
+++ src/cnn.py
@@ -13,27 +13,27 @@ TRAIN_FILE = "../resources/data/train_tweets.txt"
 SML_TRAIN_FILE = "../resources/data/test.txt"
 TEST_FILE = "../resources/data/test_tweets_unlabeled.txt"
 
-#Maximum length. Longer gets chopped. Shorter gets padded.
+# Maximum length. Longer gets chopped. Shorter gets padded.
 maxlen = 140
 
-#Model params
-#Filters for conv layers
+# Model params
+# Filters for conv layers
 nb_filter = 500
-#Number of units in the dense layer
+# Number of units in the dense layer
 dense_outputs = 256
-#Conv layer kernel size
+# Conv layer kernel size
 filter_kernels = [3, 4, 5]
-#Number of units in the final output layer. Number of classes.
+# Number of units in the final output layer. Number of classes.
 cat_output = 22
 
-#Compile/fit params
+# Compile/fit params
 batch_size = 32
 nb_epoch = 20
 
 
 def create_vocab_set():
     alphabet = (list(string.ascii_lowercase) + list(string.digits) +
-                list(string.punctuation) + ['\n'] + [' '] )
+                list(string.punctuation) + ['\n'] + [' '])
     vocab_size = len(alphabet)
     check = set(alphabet)
 
@@ -45,6 +45,7 @@ def create_vocab_set():
 
     return vocab, reverse_vocab, vocab_size, check
 
+
 def bigram_encode(x, maxlen, vocab, vocab_size, check):
 
     input_data = np.zeros((len(x), maxlen))
@@ -54,16 +55,17 @@ def bigram_encode(x, maxlen, vocab, vocab_size, check):
         chars2 = []
         for i in range(len(chars)-1):
             chars2.append(chars[i] + chars[i+1])
-        for i,c in enumerate(chars2):
+        for i, c in enumerate(chars2):
             if counter >= maxlen:
                 pass
             else:
                 if c in check:
                     counter += 1
                     ix = vocab[c]
-                    input_data[dix,counter-1] = ix
+                    input_data[dix, counter-1] = ix
 
     return input_data
+
 
 def mini_batch_generator(x, y, vocab, vocab_size, vocab_check, maxlen,
                          batch_size=128):
@@ -73,50 +75,44 @@ def mini_batch_generator(x, y, vocab, vocab_size, vocab_check, maxlen,
         y_sample = y[i:i + batch_size]
 
         input_data = bigram_encode(x_sample, maxlen, vocab, vocab_size,
-                                 vocab_check)
+                                   vocab_check)
         yield (input_data, y_sample)
 
 
 def shuffle_matrix(x, y):
-    print("x")
-    print(x)
-    print("len(x)",len(x))
-    print("x.size",x.size)
-    print("type(x)",type(x))
+    # Convert x and y to dictionary, whose key is the index string, and value
+    # is the value of x and y.
+    x_dictionary = dict([(str(index), value) for index, value in x.items()])
+    y_dictionary = dict([(str(index), value) for index, value in y.items()])
 
-
-    print("y")
-    print(y)
-    print("y.size",y.size)
-    print("type(y)",type(y))
-    print("y.values", y.values)
-
-    z = [i for i in range(len(x))]
-
-
+    # List of index to be shuffled.
+    z = list(x_dictionary.keys())
     np.random.shuffle(z)
 
-    shuffled_x = []
-    shuffled_y = []
+    shuffled_x_dict = {}
+    shuffled_y_dict = {}
+
     for i in z:
-        shuffled_x.append(x.iloc[i, :])
-        shuffled_y.append(y.iloc[i, :])
+        key_i = str(i)
+        shuffled_x_dict[key_i] = x_dictionary[key_i]
+        shuffled_y_dict[key_i] = y_dictionary[key_i]
 
+    # Convert dictionaries to pandans Series.
+    shuffled_x_dict_series = pd.Series(shuffled_x_dict)
+    shuffled_y_dict_series = pd.Series(shuffled_y_dict)
 
-
-    return shuffled_x, shuffled_y
-    # return xi, yi
+    return shuffled_x_dict_series, shuffled_y_dict_series
 
 
 def model(filter_kernels, dense_outputs, maxlen, vocab_size, nb_filter,
           cat_output):                                  # For Character Embedding use this model instead of above model
-    d = 300             #Embedding Size
-    Embedding_layer  = Embedding(vocab_size+1, d, input_length=maxlen)
+    d = 300  # Embedding Size
+    Embedding_layer = Embedding(vocab_size+1, d, input_length=maxlen)
     inputs = Input(shape=(maxlen,), name='input', dtype='float32')
     embed = Embedding_layer(inputs)
     conv = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[0],
-                             border_mode='valid', activation='relu',
-                             input_shape=(maxlen, d))(embed)
+                         border_mode='valid', activation='relu',
+                         input_shape=(maxlen, d))(embed)
     conv = MaxPooling1D(pool_length=3)(conv)
 
     conv1 = Convolution1D(nb_filter=nb_filter, filter_length=filter_kernels[1],
@@ -137,7 +133,7 @@ def model(filter_kernels, dense_outputs, maxlen, vocab_size, nb_filter,
     conv5 = MaxPooling1D(pool_length=3)(conv2)
     conv = Flatten()(conv)
 
-    #Two dense layers with dropout of .5
+    # Two dense layers with dropout of .5
     z = Dropout(0.5)(Dense(dense_outputs, activation='relu')(conv))
     # z = Dropout(0.5)(Dense(dense_outputs, activation='relu')(z))
 
@@ -152,11 +148,10 @@ def model(filter_kernels, dense_outputs, maxlen, vocab_size, nb_filter,
     return model
 
 
-
-
 def preprocess(df):
-    result_df = df.replace(to_replace ='\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', value = '@url', regex=True)
-    result_df = result_df.replace(to_replace=' ', value = '_', regex=True)
+    result_df = df.replace(
+        to_replace='\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', value='@url', regex=True)
+    result_df = result_df.replace(to_replace=' ', value='_', regex=True)
 
     X = result_df['Text']
     Y = result_df['ID']
@@ -167,14 +162,15 @@ def preprocess(df):
 
 
 print("reading file")
-raw_train_df = pd.read_csv(SML_TRAIN_FILE, delimiter='\t', header=None, names=['ID','Text'])
+raw_train_df = pd.read_csv(
+    SML_TRAIN_FILE, delimiter='\t', header=None, names=['ID', 'Text'])
 vocab, reverse_vocab, vocab_size, check = create_vocab_set()
 print("preprocessing")
 X_train, X_test, y_train, y_test = preprocess(raw_train_df)
 
 print("building model...")
 model = model(filter_kernels, dense_outputs, maxlen, vocab_size,
-                       nb_filter, cat_output)
+              nb_filter, cat_output)
 
 print("fitting model...")
 initial = datetime.datetime.now()
@@ -184,12 +180,12 @@ for e in range(nb_epoch):
     xi_test, yi_test = shuffle_matrix(X_test, y_test)
 
     batches = mini_batch_generator(xi, yi, vocab, vocab_size,
-                                                   check, maxlen,
-                                                   batch_size=batch_size)
+                                   check, maxlen,
+                                   batch_size=batch_size)
 
     test_batches = mini_batch_generator(xi_test, yi_test, vocab,
-                                                    vocab_size, check, maxlen,
-                                                    batch_size=batch_size)
+                                        vocab_size, check, maxlen,
+                                        batch_size=batch_size)
 
     accuracy = 0.0
     loss = 0.0
@@ -224,7 +220,3 @@ for e in range(nb_epoch):
     print(
         'Epoch {}. Loss: {}. Accuracy: {}\nEpoch time: {}. Total time: {}\n'.format(e, test_loss_avg, test_accuracy_avg,
                                                                                     e_elap, t_elap))
-
-
-
-
